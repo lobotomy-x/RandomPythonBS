c6f1611ddb9bd52c3f1ae186edec8b0242b8fedd
lobotomyx <lbatvt@gmail.com>
2024-11-26 13:08:20 -0500

    Update uevrsussystart.py

--- uevrsussystart.py
+++ uevrsussystart.py
@@ -1,11 +1,22 @@
 import os
 import psutil
 import pyinjector
-from PyQt5 import QtWidgets, QtGui, QtCore
 import subprocess
 import sys
 from shutil import copyfile
 
+
+#automatically retrieves uevr dlls from your path with no hassle and manages starting and stopping the frontend
+
+#should suffice for most games without anticheat. for games with it there can be issues. I have a much more powerful loader in the works that just isnt ready for public release so for now throwing this out there
+
+#if you need to get 3dmigoto into a uevr game just put it in uevr plugins folder and use this
+
+#if starting from shortcut, bat, or cmd first arg is gamepath everything else is game args, otherwise use interactively
+
+#can handle double quotes so you can right click a game file and use copy as path without havint to delete the quotes
+
+#normally will inject openxr_loader but if you type "late" as an arg it will instead copy openxrloader into the gamedir after startup and you will have to manually click reinitialize runtime. This works and is needed for some games
 
 dlls = []
 init_dir = os.path.dirname(sys.argv[0])
