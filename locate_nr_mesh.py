ddce29084803c055536c4611bbdceb933b412f70
lobotomyx <lbatvt@gmail.com>
2025-02-23 02:05:56 -0500

    Update and rename locate_mesh.py to locate_nr_mesh.py
    fix for case when loading files not from current directory (technically works for multiple dirs but thats only possible if choosing files from a search result), auto installs dependency

--- locate_nr_mesh.py
+++ locate_nr_mesh.py
@@ -1,10 +1,19 @@
-from os import scandir, rename, makedirs, getcwd, system
+from os import scandir, rename, makedirs, getcwd, system, chdir
 from os.path import join, isdir, isfile, dirname, splitext
 from sys import argv
-import binary2strings as b2s
 from tkinter import Tk
 import tkinter as tk
 import tkinter.filedialog as fd
+
+try:
+    import binary2strings as b2s
+except Exception as e:
+    system("pip install binary2strings")
+    import binary2strings as b2s
+
+
+
+
 def print_strings_tofile(path = None, filetype = None, interesting = True):
     spath = getcwd() if path is None else path
     files = []
@@ -41,6 +50,9 @@ root.withdraw()
 root = tk.Tk()
 textures = fd.askopenfilenames(parent=root, title='Choose textures',
                             filetypes=[("textures", (".png",".bmp", ".jpg", ".dds"))])
-tex = [f.rsplit("/")[-1] for f in textures]
-print(tex)
-search_strings(tex,filetype=(".nr"))
+_dirs = [dirname(f) for f in textures]
+dirs = list(set(_dirs))
+for _dir in dirs:
+    chdir(_dir)
+    tex = [f.rsplit("/")[-1] for f in textures if dirname(f) == _dir]
+    search_strings(tex,filetype=(".nr"))
