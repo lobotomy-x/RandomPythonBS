5feb352cb7fd0e8a872fbb26f9bd9f8ce5c509fb
lobotomyx <lbatvt@gmail.com>
2025-02-23 02:13:46 -0500

    Update locate_nr_mesh.py
    remove unneeded func

--- locate_nr_mesh.py
+++ locate_nr_mesh.py
@@ -13,23 +13,6 @@ except Exception as e:
 
 
 
-
-def print_strings_tofile(path = None, filetype = None, interesting = True):
-    spath = getcwd() if path is None else path
-    files = []
-    if filetype is not None:
-        files = [f.path for f in scandir(spath) if f.path.lower().endswith(tuple([j.lower() for j in filetype]))]
-    else:
-        files = [f.path for f in scandir(spath)]
-    for f in files:
-        bindat = open(f, "rb").read()
-        text = b2s.extract_all_strings(bindat, min_chars=4, only_interesting=interesting)
-        with open(join(getcwd(), str(f).replace(splitext(f)[1],".txt")), "a", encoding="utf-8") as otxt:
-            textout = ""
-            for string in text:
-                if string[1] == "UTF8":
-                    print(string[0], file=otxt)
-
 def search_strings(search_terms, path = None, filetype = None, interesting = True):
     spath = getcwd() if path is None else path
     files = []
