912dab0927c717134ceec9c38befd8f2ed4f1379
lobotomyx <lbatvt@gmail.com>
2025-03-06 12:03:36 -0500

    ue mod folder maker
    put next to unreal engine modded files, click script, check root of drive for full folder structure and files (no subdirs) copied
    must have dumped files in "Exports", e.g. "H:\dump\Exports\Client\Content\Blueprints\swag.uasset" would give you "H:\Client\Content\Blueprints\swag.uasset"

---
+++ modfoldermaker.py
@@ -0,0 +1,16 @@
+from os import scandir, rename, makedirs, getcwd, system
+from os.path import abspath, relpath, basename, join, isdir, isfile, dirname
+from sys import argv
+from shutil import copyfile
+modcontent = []
+if len(argv) > 1:
+    modcontent = argv[1]
+else:
+    modcontent = [f.name for f in scandir(getcwd()) if (not f.name.endswith(".py") and isfile(f))]
+exports, modpath = abspath(getcwd()).split("Exports")
+
+makedirs((modfolder:=join(getcwd(),"Mod", modpath)), exist_ok = True)
+
+for modfile in modcontent:
+    copyfile(abspath(modfile), dest:=join(getcwd(),modfolder, modfile))
+    print(dest)
